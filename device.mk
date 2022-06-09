a95ec113772558c0db8f9ab7ce02326a6968c4b1
Bruno Martins <bgcngm@gmail.com>
2022-06-09 14:34:59 +0100

    pyrite: Use minimal GMS if building with it
    Only minimal GMS fits under the 2G upload limit for most services.
    
    Change-Id: If9ce99880bcd6949354f80882176aaaded9aad85

--- device.mk
+++ device.mk
@@ -134,6 +134,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# GMS
+ifeq ($(WITH_GMS),true)
+GMS_MAKEFILE=gms_minimal.mk
+endif
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti:64 \
