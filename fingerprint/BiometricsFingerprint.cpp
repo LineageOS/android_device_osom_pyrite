2b00ded7897d25fb9b852e4cc124e9ad806aa975
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-04 20:44:52 -0500

    otter: Rebrand to pyrite
    Change-Id: I6d0392b167c91611949d809d4e1ffeb3587f78e9

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.otter"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.3-service.otter"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.pyrite"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.3-service.pyrite"
 
 #include <hardware/hw_auth_token.h>
 
