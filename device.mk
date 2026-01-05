9cf4715af276be8b1c46cdd8755e2498d5ea6d10
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-05 18:39:27 -0500

    pyrite: Drop local fingerprint HAL & UDFPS configs
    * Not needed.
    * Also remove inapplicable sepolicy.
    
    Change-Id: If48debb16fed7e9fe52f956dc56413dc589ca463

--- device.mk
+++ device.mk
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.pyrite
+    android.hardware.biometrics.fingerprint@2.3-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
