54cab46feceb108597913123bf2624f85c8df9dd
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-05 19:35:54 -0500

    pyrite: Update proprietary files/firmware lists
    * And extraction notices.
    * Kang IMS from otter as ours crashes on IMS Factory HAL init.
    * Kang Euicc from otter to keep it simple.
    * `sort-blobs-list --dir-first` while here.
    
    Change-Id: I408981a55dd98abfe1df90f99aac9c0a49a5fe09

--- extract-files.py
+++ extract-files.py
@@ -45,8 +45,6 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
-    'vendor/lib64/hw/fingerprint.lahaina.so': blob_fixup()
-        .fix_soname(),
     (
         'vendor/lib64/libdpps.so',
         'vendor/lib64/libsnapdragoncolor-manager.so',
