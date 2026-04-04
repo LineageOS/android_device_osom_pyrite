2804a71084f13c8f54e5770bdc96fd283e857b9f
Michael Bestas <mkbestas@lineageos.org>
2026-04-05 02:11:42 +0300

    Partially revert "pyrite: Set EOL stock SPLs, shipping LvL & VBMeta info"
    This partially reverts commit 30c14d9a3c875d13a3719de12708da86dc4be05e.
    
    Reason for revert: sm8350 board API level is 30.
    
    Change-Id: I8f19281cbe7b79206c8d8d7421258951354ded84

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Board API level
-BOARD_SHIPPING_API_LEVEL := 31
+BOARD_SHIPPING_API_LEVEL := 30
 
 # Boot control
 PRODUCT_PACKAGES += \
